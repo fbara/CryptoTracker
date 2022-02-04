2cc1933f4c8b7f50bb5c6a0c1adb56c09594400f
fbara <fbara1@yahoo.com>
2022-02-04 14:13:39 -0600

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Frank Bara on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
